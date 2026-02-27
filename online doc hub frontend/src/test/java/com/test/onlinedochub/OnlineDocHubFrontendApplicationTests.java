06cf4205d548c07acf1c95ab96bbbb93f17238cb
fredericIT <ntawukuriryayofrederic817@gmail.com>
2026-02-27 18:21:42 +0200

    added frontend

---
+++ online doc hub frontend/src/test/java/com/test/onlinedochub/OnlineDocHubFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.onlinedochub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineDocHubFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
